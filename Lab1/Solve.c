079c833e3c18d3ed8b68348e23e60c6fee6bd297
Rigorich <nikita.grishaev@mail.ru>
2020-04-09 15:03:05 +0300

    makefile test

--- Lab1/Solve.c
+++ Lab1/Solve.c
@@ -1,10 +1,10 @@
 
-#include "C:\BSUIR\C\deRigorich\deRigorich.h"
+#include "deRigorich.h"
 
 int main(void) {
 	printf("Enter four-digit number:\n");
 	int a = StringReadInt(stdin, 1000, 9999);
 	int b = a / 100 + (a % 100) * 100;
-	printf("%d -> %d\n", a, b);
+	printf("%d -> %d\n", a, b); 
 	return 0;
 }
